df779450443fe1d962f18e17c42d76e1e9b10ae0
SJH1207 <sjh576244995@gmail.com>
2023-09-23 13:33:02 +0800

    优化接口

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require("express");
+const parser = require("body-parser");
 const app = express();
-const joi = require('joi')
+const joi = require("joi");
 
 // 允许跨域
 const cors = require("cors");
 app.use(cors());
 
+/* 原生 */
+// 解析JSON格式
+// app.use(express.json())
 // 配置解析表达数据的中间件，注意：这个中间件，只能解析 application/x-www-form-urlencoded 格式的表单数据
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.urlencoded({ extended: false }));
+
+/* 第三方 */
+// 解析JSON格式
+app.use(parser.json());
+// 解析 application/x-www-form-urlencoded 格式
+// app.use(parser.urlencoded({ extended: false }));
 
 // 一定要在路由之前，封装 res.cc 函数
 app.use((req, res, next) => {
@@ -29,12 +39,12 @@ app.use("/api", userRouter);
 // 定义错误级别的中间件
 app.use((err, req, res, next) => {
   // 验证失败导致的错误
-  if (err instanceof joi.ValidationError) return res.cc(err)
+  if (err instanceof joi.ValidationError) return res.cc(err);
   // 身份认证失败后的错误
-  if (err.name === 'UnauthorizedError') return res.cc('身份认证失败！')
+  if (err.name === "UnauthorizedError") return res.cc("身份认证失败！");
   // 未知的错误
-  res.cc(err)
-})
+  res.cc(err);
+});
 
 app.listen(8080, () => {
   console.log("express is running at http://127.0.0.1:8080");
--- router_handler/user.js
+++ router_handler/user.js
@@ -3,18 +3,18 @@ const db = require("../db/index");
 // 导入 bcryptjs 这个包
 const bcrypt = require("bcryptjs");
 // // 导入生成 Token 的包
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 // // 导入全局的配置文件
-const config = require('../config')
+const config = require("../config");
 
 // 注册新用户的处理函数
 exports.regUser = (req, res) => {
   const userInfo = req.body;
   // 校验为空
-  // if (!userInfo.username || !userInfo.password) {
-  //   // return res.send({ code: 400,  msg: "username或password为空" });
-  //   return res.cc('username或password为空')
-  // }
+  if (!userInfo.username || !userInfo.password) {
+    // return res.send({ code: 400,  msg: "username或password为空" });
+    return res.cc("username或password为空");
+  }
 
   // 是否存在
   const sqlStr = "select * from user where username=?";
@@ -26,7 +26,7 @@ exports.regUser = (req, res) => {
 
     if (result.length > 0) {
       // return res.send({ code: 400, msg: "该用户名已被占用,请更换其他用户名!" });
-      return res.cc('该用户名已被占用,请更换其他用户名!')
+      return res.cc("该用户名已被占用,请更换其他用户名!");
     }
 
     // 加密密码
@@ -37,14 +37,14 @@ exports.regUser = (req, res) => {
     db.query(addsql, userInfo, (error, result) => {
       if (error) {
         // return res.send({ code: 400, msg: error.message });
-        return res.cc(error)
+        return res.cc(error);
       }
       if (result.affectedRows !== 1) {
         // return res.send({ code: 400, msg: "注册用户失败，请稍后再试！" });
-        return res.cc("注册用户失败，请稍后再试！")
+        return res.cc("注册用户失败，请稍后再试！");
       }
       // res.send({ code: 200, msg: "注册成功!" });
-      res.cc('注册成功!',200)
+      res.cc("注册成功!", 200);
     });
   });
 };
@@ -52,29 +52,42 @@ exports.regUser = (req, res) => {
 // 登录的处理函数
 exports.login = (req, res) => {
   // 接收表单的数据
-  const userinfo = req.body
+  const userInfo = req.body;
+  console.log("userInfo", req.body);
+
+  // 校验为空
+  if (!userInfo.username || !userInfo.password) {
+    // return res.send({ code: 400,  msg: "username或password为空" });
+    return res.cc("username或password为空");
+  }
+
   // 定义 SQL 语句
-  const sql = `select * from user where username=?`
+  const sql = `select * from user where username=?`;
   // 执行 SQL 语句，根据用户名查询用户的信息
-  db.query(sql, userinfo.username, (err, results) => {
+  db.query(sql, userInfo.username, (err, results) => {
     // 执行 SQL 语句失败
-    if (err) return res.cc(err)
+    if (err) return res.cc(err);
     // 执行 SQL 语句成功，但是获取到的数据条数不等于 1
-    if (results.length !== 1) return res.cc('登录失败！')
+    if (results.length !== 1) return res.cc("登录失败！");
 
     // TODO：判断密码是否正确
-    const compareResult = bcrypt.compareSync(userinfo.password, results[0].password)
-    if (!compareResult) return res.cc('登录失败！')
+    const compareResult = bcrypt.compareSync(
+      userInfo.password,
+      results[0].password
+    );
+    if (!compareResult) return res.cc("登录失败！");
 
     // TODO：在服务器端生成 Token 的字符串
-    const user = { ...results[0], password: '', user_pic: '' }
+    const user = { ...results[0], password: "", user_pic: "" };
     // 对用户的信息进行加密，生成 Token 字符串
-    const tokenStr = jwt.sign(user, config.jwtSecretKey, { expiresIn: config.expiresIn })
+    const tokenStr = jwt.sign(user, config.jwtSecretKey, {
+      expiresIn: config.expiresIn,
+    });
     // 调用 res.send() 将 Token 响应给客户端
     res.send({
       status: 200,
-      message: '登录成功！',
-      token:  'Bearer ' + tokenStr,
-    })
-  })
-}
+      message: "登录成功！",
+      token: "Bearer " + tokenStr,
+    });
+  });
+};
