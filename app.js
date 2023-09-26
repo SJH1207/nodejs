4af4b0d53d49f75fb581a34aed8cd540202d0d66
SJH1207 <sjh576244995@gmail.com>
2023-09-26 19:13:31 +0800

    优化

--- app.js
+++ app.js
@@ -19,22 +19,12 @@ app.use(parser.json());
 // 解析 application/x-www-form-urlencoded 格式
 // app.use(parser.urlencoded({ extended: false }));
 
-// 一定要在路由之前，封装 res.cc 函数
-app.use((req, res, next) => {
-  // status 默认值为 400，表示失败的情况
-  // msg 的值，可能是一个错误对象，也可能是一个错误的描述字符串，也可能是正确的番号提示
-  res.cc = function (msg, status = 400) {
-    res.send({
-      status,
-      msg: msg instanceof Error ? msg.message : msg,
-    });
-  };
-  next();
-});
-
 // 导入并使用用户路由模块
 const userRouter = require("./router/user");
 app.use("/api", userRouter);
+
+const codeRouter = require("./router/code");
+app.use("/api", codeRouter);
 
 app.listen(8080, () => {
   console.log("express is running at http://127.0.0.1:8080");
--- router_handler/user.js
+++ router_handler/user.js
@@ -59,7 +59,7 @@ exports.login = (req, res) => {
   // 执行 SQL 语句，根据用户名查询用户的信息
   db.query(sql, userInfo.username, (err, results) => {
     // 执行 SQL 语句失败
-    if (err) return res.cc(err);
+    if (err) return res.send({ code: 400, msg: error.message });
     // 执行 SQL 语句成功，但是获取到的数据条数不等于 1
     if (results.length !== 1) return res.send({ code: 400,  msg: "登录失败！" });
     // TODO：判断密码是否正确
